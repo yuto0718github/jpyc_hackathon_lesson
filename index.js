e94309a183b98de41b3b89ab18eb910fa3330a96
yuto0718github <yuto.y.0718@gmail.com>
2022-04-03 19:20:32 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 console.log("hello world");
+async function my Function(){
+  const provider = await new ethers.providers.Web3Provider(window.ethereum) 
+  const blockNumber = await provider.getBlockNumber();
+  alert(blockNumber);
+}
